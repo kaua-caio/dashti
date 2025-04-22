2e972d2316fbfd205b8943d8fb8b88e6c0c05034
kaua-caio <kaua.lima@greenn.com.br>
2025-04-22 17:56:40 -0300

    adicionado novas frases

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const greetingEl = document.getElementById('greeting');
 function getGreeting() {
     const hour = new Date().getHours();
 
-    if (hour < 12) return "Bom dia!";
-    if (hour < 18) return "Boa tarde!";
-    return "Boa noite!";
+    if (hour < 12) return "Olá, Bom dia!";
+    if (hour < 18) return "Olá, Boa tarde!";
+    return "Olá, Boa noite!";
 }
 
 greetingEl.textContent = getGreeting();
